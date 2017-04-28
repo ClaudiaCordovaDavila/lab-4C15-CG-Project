d1b996bd5a6c466c5e2ccc7fa0544b547f6d023c
Gustavoch25 <gustavo.chavez.c@tecsup.edu.pe>
2017-04-28 17:07:22 -0500

    cambio

--- Lab10_CVersiones/src/com/tecsup/lab9/git/controller/StudentCtrl.java
+++ Lab10_CVersiones/src/com/tecsup/lab9/git/controller/StudentCtrl.java
@@ -3,5 +3,6 @@ package com.tecsup.lab9.git.controller;
 public class StudentCtrl {
 	public static void main(String[] args) {
 	System.out.println("Hola Mundo");
+	System.out.println("Hola Mundo2");
 	}
 }
