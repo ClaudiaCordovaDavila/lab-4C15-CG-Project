928094dedd3a82aaeccbf987ef0a5e11abfdff92
ClaudiaCordovaDavila <claudia.cordova@tecsup.edu.pe>
2017-04-28 17:07:20 -0500

    cambio

--- Lab10_CVersiones/src/com/tecsup/lab9/git/controller/StudentCtrl.java
+++ Lab10_CVersiones/src/com/tecsup/lab9/git/controller/StudentCtrl.java
@@ -3,5 +3,7 @@ package com.tecsup.lab9.git.controller;
 public class StudentCtrl {
 	public static void main(String[] args) {
 	System.out.println("Hola Mundo");
+	
+	System.out.println("Hola Gustavo");
 	}
 }
