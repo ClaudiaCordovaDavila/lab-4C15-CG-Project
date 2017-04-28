69e36660b9c4ae6190914e5757eab7d7ef99fe1c
ClaudiaCordovaDavila <claudia.cordova@tecsup.edu.pe>
2017-04-28 17:30:56 -0500

    sum

--- Lab10_CVersiones/src/com/tecsup/lab9/git/controller/StudentCtrl.java
+++ Lab10_CVersiones/src/com/tecsup/lab9/git/controller/StudentCtrl.java
@@ -3,8 +3,16 @@ package com.tecsup.lab9.git.controller;
 public class StudentCtrl {
 	public static void main(String[] args) {
 	System.out.println("Hola Mundo");
-	System.out.println("Hola Mundo2");
-	
+	System.out.println("Hola Mundo2");	
 	System.out.println("Holaaaaaaa");
+	
+	}
+	public void Sumar(){
+		int a = 5;
+		int b = 3;
+		int suma; 		
+		suma = a+b;
+		
+		System.out.println(suma);
 	}
 }
